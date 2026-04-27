97df52fec2e1648ebbe584709aa08de532b65997
OLAOLUWA-BAMIDELE <deleyiwola@gmail.com>
2026-04-27 11:34:24 +0100

    Refactor booking update logic

--- src/main/java/dan/springframework/eventbooker/entity/Event.java
+++ src/main/java/dan/springframework/eventbooker/entity/Event.java
@@ -2,9 +2,6 @@ package dan.springframework.eventbooker.entity;
 
 import com.fasterxml.jackson.annotation.JsonManagedReference;
 import jakarta.persistence.*;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
 import lombok.*;
 
 import java.time.LocalDateTime;
