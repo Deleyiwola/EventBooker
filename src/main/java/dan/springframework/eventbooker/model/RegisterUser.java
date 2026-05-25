f3db4773767c652f107dfdb1685d842c0f39d4d3
OLAOLUWA-BAMIDELE <deleyiwola@gmail.com>
2026-05-25 13:35:28 +0100

    Merge remote-tracking branch 'origin/databases/postgresql' into databases/postgresql

--- src/main/java/dan/springframework/eventbooker/model/RegisterUser.java
+++ src/main/java/dan/springframework/eventbooker/model/RegisterUser.java
@@ -1,15 +1,29 @@
 package dan.springframework.eventbooker.model;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 public class RegisterUser {
+    @NotBlank
     private String phoneNumber;
+
+    @NotBlank
     private String firstName;
+
+    @NotBlank
     private String lastName;
+
+    @NotBlank
     private String email;
+
+    @NotBlank
+    @Size(min = 8, message = "Password must be at least 8 characters")
     private String password;
+
+    @NotBlank
     private String confirmPassword;
 }
