c962fbad235dc88b017656f50e7e0fc41434dabc
OLAOLUWA-BAMIDELE <deleyiwola@gmail.com>
2026-06-16 15:00:01 +0100

    Merge remote-tracking branch 'origin/databases/postgresql' into databases/postgresql

--- src/main/java/dan/springframework/eventbooker/controller/BookingController.java
+++ src/main/java/dan/springframework/eventbooker/controller/BookingController.java
@@ -27,7 +27,7 @@ public class BookingController {
 
     private final BookingService bookingService;
 
-    @PreAuthorize("hasRole('ORGANIZER')")
+    @PreAuthorize("hasRole('ADMIN')")
     @GetMapping(BOOKING_URI)
     public List<BookingDTO> listBookings() {
         return bookingService.getBookings();
@@ -50,9 +50,9 @@ public class BookingController {
 
     @PreAuthorize("hasRole('ORGANIZER')")
     @GetMapping(BOOKING_EVENT_ID_URI)
-    public List<BookingDTO> getBookingsByEventId(@PathVariable Long eventId) {
+    public List<BookingDTO> getBookingsByEventId(@PathVariable Long eventId, Authentication authentication) {
         log.info("Getting booking with eventId {}", eventId);
-        return bookingService.getBookingsByEventId(eventId);
+        return bookingService.getBookingsByEventId(eventId, ((User)authentication.getPrincipal()).getEmail() );
     }
 
     @PostMapping(BOOKING_URI)
